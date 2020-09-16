a60f2876645a75cdf9fd10fb6e882b1159970f26
tucucrespi <tucucrespi@hotmail.com>
2020-09-16 19:35:10 +0000

    add 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,4 +12,4 @@ printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 prinft("Size of a long long int: %ld byte(s)\n", sizeof(long long));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
-
+}
