1d2a015ed48ea75dca9ff7f3dd3b1b948a8f70dd
tucucrespi <tucucrespi@hotmail.com>
2020-09-22 17:57:37 +0000

    add more

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - print the sum of two integer.
+ * @a: firts int
+ * @b: second int
+ * Return: result int.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 9 ; a++)
+	{
+		for(b = 0; b < 9; b++)
+		{
+			c= a * b;
+			if (c < 9)
+			{
+				_putchar ((c / 10)+ 47);
+				_putchar ((c / %10));
+				_putchar ((','));
+				_putchar (' ');
+			}
+		}
+	}
+}
