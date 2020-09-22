f27ee998702dbf3ef9ea8e052422f037f4fc8328
tucucrespi <tucucrespi@hotmail.com>
2020-09-22 01:35:33 +0000

    add more

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+ * _islower - check if c is lowercase
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, 0 is returned, and errno is set appropriately.
+ */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,12 @@
 #include "holberton.h"
+/**
+ * _isalpha - checks if c is lower or uppercase
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, 0 is returned, and errno is set appropriately.
+ */
+
 int _isalpha(int c)
 {
 if (c >= 97 && c <= 122)
