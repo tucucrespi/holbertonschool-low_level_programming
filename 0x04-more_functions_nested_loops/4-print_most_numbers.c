999dfccc5763fb168071b9e443773421c7b0e048
tucucrespi <tucucrespi@hotmail.com>
2020-09-24 14:00:02 +0000

    add

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,18 @@
 #include "holberton.h"
 /**
- * _isupper - check if c is lowercase
- * @c: The character to print
+ * print_most_numbers - print numbers 0 to 9 exept 2 and 4
+ * The character to print
  *
  * Return: On success 1.
  * On error, 0 is returned, and errno is set appropriately.
  */
 void print_most_numbers(void)
 {
+char x;
+for (x = '0'; x <= '9'; x++)
+{
+if (x != '2' && x != '4')
+_putchar (x);
+}
+_putchar ('\n');
+}
