56260231cc6164d4f2d2d4db9a5d0e27c9031781
tucucrespi <tucucrespi@hotmail.com>
2020-09-24 19:04:12 +0000

    add

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,16 +3,18 @@
 int main(void)
 {
 	int i;
-	for (i=1; i<=100; i++)
+	for(i=1; i<=100; i++)
 	{
-		if (i%15 == 0)
-			printf ("FizzBuzz\t");
-		else if ((i%3) == 0)
-			printf("Fizz\t");
-		else if ((i%5) == 0)
-			printf("Buzz\t");
+		if(((i%3)||(i%5))== 0)
+			printf("number= %d FizzBuzz\n", i);
+		else if((i%3)==0)
+			printf("number= %d Fizz\n", i);
+		else if((i%5)==0)
+			printf("number= %d Buzz\n", i);
 		else
-			printf("%d\t", i);
+			printf("number= %d\n",i);
+
 	}
+
 	return 0;
 }
