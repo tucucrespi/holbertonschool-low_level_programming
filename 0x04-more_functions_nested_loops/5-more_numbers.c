f42aeaeaa9adad1896162800f75eeb548ae611d4
tucucrespi <tucucrespi@hotmail.com>
2020-09-24 14:55:29 +0000

    add

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print the sum of two integer.
+ * print numbers 0 to 9
+ *
+ * Return: result int.
+ */
+void more_numbers(void)
+{
+char x;
+char p;
+
+for (x = 0 ; x <= 9; x++)
+{
+for (p = 0; p <= 14; p++)
+{
+if (x > 9)
+{
+_putchar (x / 10 + '0');
+_putchar (p % 10 + '0');
+}
+_putchar ('\n');
+}
+}
