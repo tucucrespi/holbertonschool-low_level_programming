d622510597146dda617769a0172815cd0ff28e0a
tucucrespi <tucucrespi@hotmail.com>
2020-09-29 01:17:22 +0000

    add more

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,23 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * print_rev - returns a string rev.
- * @s : s is words
+ * @s: s is words
  * Return: quantiti is i
  */
 
 void print_rev(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; --i)
+int count;
+char letter;
+
+for (count = 0; s[count] != '\0'; count++)
 {
-_putchar(s[i]);
 }
-_putchar ('\n');
+for (count = count - 1; count >= 0 ; count--)
+{
+letter = s[count];
+_putchar(letter);
+}
+_putchar('\n');
 }
