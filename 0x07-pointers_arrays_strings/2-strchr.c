9667a74d752761f5f6ba56e3c61c559d1f01ef58
tucucrespi <tucucrespi@hotmail.com>
2020-10-05 23:49:25 +0000

    add

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,14 @@
 
 char *_strchr(char *s, char c)
 {
-int i;
-
-for (i = 0; s[i] != '\0'; i++)
+while (*s != c && *s != '\0')
 {
-if (s[i] == c)
+s++;
+}
+if (*s == c)
 {
-s = &s[i];
 return (s);
 }
-}
+else
 return (0);
 }
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * *_strchr - prints buffer in hexa
+ * @s: the address of memory to print
+ * @accept: the size of the memory to print
+ * Return: Nothing.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int c, i;
+int j = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+c++;
+break;
+}
+}
+if (accept[j] == '\0')
+break;
+}
+return (c);
+}
