b81b7d66fe9075e408c98357ec54d30ed9081ea4
tucucrespi <tucucrespi@hotmail.com>
2020-10-07 22:31:02 +0000

    add

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *find_prime - prints buffer in hexa
+ * @x: the address of memory to print
+ * @y: the address of memory to print
+ * Return: Nothing.
+ */
+int find_prime(int x, int y)
+{
+  if (x < 2)
+    return (0);
+  if (y * y > x)
+    return (1);
+  if (x % y == 0)
+    return (0);
+  else 
+    return (find_prime(x, y + 1));
+}
+/**
+ *is_prime_number(int n)
+ *@n:the address of memory to print
+ * Return: Nothing.
+ */
+int is_prime_number(int n)
+{
+  return (find_prime(n, 2));
+}
