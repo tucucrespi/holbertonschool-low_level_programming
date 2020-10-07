7c9f3fdc74832c5d7933fc665f3623bdeb4cbcd9
tucucrespi <tucucrespi@hotmail.com>
2020-10-07 14:10:25 +0000

    add

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - prints buffer in hexa
+ * @s: the address of memory to print
+ * Return: Nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar ('\n');
+return;
+}
+else
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
