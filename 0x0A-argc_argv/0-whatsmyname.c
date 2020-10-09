10976c0777b79c70905ef7f42449dd67de195fe0
tucucrespi <tucucrespi@hotmail.com>
2020-10-09 14:29:03 +0000

    add

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/** main -main entry
+ *
+ * @argc: number of command line arguments
+ * @argv: numbar of command line argumetns
+ *Return:integer
+ **/
+int main (int argc, char *argv[])
+{
+	(void)argc;
+	printf ("%s\n", argv[0]);
+	return (0);
+}
