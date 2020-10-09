64cb9ed6c4eae9e787ba6b10525115c406e3ce9b
tucucrespi <tucucrespi@hotmail.com>
2020-10-09 20:55:20 +0000

    add

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints buffer in hexa
+ * @argc: number of command line arguments
+ * @argv: number of command line arguments
+ * Return: Nothing.
+ */
+
