2c95f1be8157379da4c3ac324380933f4f226f24
tucucrespi <tucucrespi@hotmail.com>
2020-10-09 19:04:37 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints buffer in hexa
+ * @argc: number of command line arguments
+ * @argv: number of command line arguments
+ * Return: Nothing.
+ */
+
+int main(int argc, char *argv[])
+{
+int i, a, sum = 0;
+for (i = 1; i < argc; i++)
+{
+a = atoi(argv[i]);
+if (a == 0 && *argv[i] != '0')
+{
+printf("Error\n");
+return (1);
+}
+sum += a;
+}
+printf("%d\n", sum);
+return (0);
+}
