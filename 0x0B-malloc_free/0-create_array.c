09c5b4456da01e7f549f601579599dceed691118
tucucrespi <tucucrespi@hotmail.com>
2020-10-14 22:19:27 +0000

    add

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,17 +2,34 @@
 #include <stdlib.h>
 
 /**
- * *create_array - prints buffer in hexa
- * @c: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * create_array - creates an array of chars
+ * @size: int type
+ * @c: Char type
+ * Return: a pointer to the array, or NULL if it fails
  */
 
 char *create_array(unsigned int size, char c)
 {
-int i
-	char *parray;
-if (size != 0)
+char *a;
+unsigned int i;
+
+if (size == 0)
 {
-	parray = (char *)malloc(sizeof(char) * size);
+return (NULL);
+}
+a = malloc(sizeof(char) * size);
+
+if (a == NULL)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+a[i] = c;
+}
+}
+return (a);
+
+}
