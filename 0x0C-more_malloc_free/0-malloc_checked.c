fd0063f1cc31a495a809df968712d19aad34d6e5
tucucrespi <tucucrespi@hotmail.com>
2020-10-15 03:38:29 +0000

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory
+ * @b: unsigned int type
+ * Return: pointer
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
