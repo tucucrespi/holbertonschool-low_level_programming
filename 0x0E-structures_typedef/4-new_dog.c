1ddb271b2c19f04b1549078530e40ba327b895b8
tucucrespi <tucucrespi@hotmail.com>
2020-10-19 20:06:19 +0000

    add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * new dog - return name of the file asdasgassgasg
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,10 +10,10 @@
 
 void free_dog(dog_t *d)
 {
-if (d == NULL)
+if (d == '\0')
 return;
 
-free(d->owner);
 free(d->name);
+free(d->owner);
 free(d);
 }
