e6b84f5d4b8cb001614457a6c56a6ec18816b316
tucucrespi <tucucrespi@hotmail.com>
2020-10-21 17:19:59 +0000

    add

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * int_index - sog
+ * @array: name of dog
+ * @size: age of dog
+ * @cmp: owner of dog
+ * Return: return always 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
