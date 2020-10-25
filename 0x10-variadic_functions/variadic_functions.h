1a66f778f1e594c26a3ebcadbc10992a5f429eba
tucucrespi <70914357+tucucrespi@users.noreply.github.com>
2020-10-25 12:41:12 -0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,26 +1,24 @@
-#ifndef VARIADIC_FUNCIONS_H
-#define VARIADIC_FUNCIONS_H
+#ifndef VARIADIC_H
+#define VARIADIC_H
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+int _putchar(char c);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
+void print_char(va_list argumentList);
+void print_int(va_list argumentList);
+void print_float(va_list argumentList);
+void print_string(va_list argumentList);
 
 /**
- * struct op
- * @c: char name print
- * @callfunction: function to print
+ *struct op - struct op
+ *@c: string
+ *@call_function: pointer to function
  */
-
-
 typedef struct op
 {
-  char *c;
-  void (*call_function)();
-}op;
-
+	char *c;
+	void (*call_function)();
+} op;
 #endif
