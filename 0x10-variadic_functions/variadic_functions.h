f628b40eebe130ac1dd847d19ab60cd5d1406856
tucucrespi <tucucrespi@hotmail.com>
2020-10-25 15:20:25 +0000

    add

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,13 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct op
+ * @c: char name print
+ * @callfunction: function to print
+ */
+
+
 typedef struct op
 {
   char *c;
