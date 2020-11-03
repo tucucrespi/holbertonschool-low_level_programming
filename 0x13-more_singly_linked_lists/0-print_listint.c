336b6e85a79d7a109e3cf8ff30cae9acd6d9d8fa
tucucrespi <tucucrespi@hotmail.com>
2020-11-03 15:51:26 +0000

    add

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * print_listint - singly linked list
+ * @h: integer
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * Return: Holberton project
  */
 size_t print_listint(const listint_t *h)
 {
-	int i;
-	for(i = 0; h != NULL; i++)
-	{
-		printf("%i\n", h-> n);
-		h= h->next;
-	}
-	return(i);
+int i;
+for (i = 0; h != NULL; i++)
+{
+printf("%i\n", h->n);
+h = h->next;
+}
+return (i);
 }
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * listint_len - singly linked list
+ * @h: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+size_t listint_len(const listint_t *h)
+{
