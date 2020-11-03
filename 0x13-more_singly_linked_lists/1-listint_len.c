163447711d84abf9342537ba8514638873b5e916
tucucrespi <tucucrespi@hotmail.com>
2020-11-03 17:40:29 +0000

    add

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,10 +2,20 @@
 /**
  * listint_len - singly linked list
  * @h: integer
- * @next: points to the next node
- *
+ *  *
  * Description: singly linked list node structure
- * for Holberton project
+ * Return: Holberton project
  */
 size_t listint_len(const listint_t *h)
 {
+unsigned int contador = 0;
+
+if (!h)
+return (0);
+while (h)
+{
+h = h->next;
+contador++;
+}
+return (contador);
+}
