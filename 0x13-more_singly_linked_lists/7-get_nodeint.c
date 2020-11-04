64b8b812b86d77752361d2767bc90c7b4ddfc547
tucucrespi <tucucrespi@hotmail.com>
2020-11-04 19:06:47 +0000

    add

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - print the last node.
+ * @head: the inital pointer to the linked list
+ * @index: integer
+ * Return: the new node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+listint_t *tmp = head;
+
+if (tmp == NULL)
+return (NULL);
+
+while (i < index)
+{
+tmp = tmp->next;
+if (tmp == NULL)
+return (NULL);
+i++;
+}
+return (tmp);
+}
