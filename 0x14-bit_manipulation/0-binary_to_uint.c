5e2f3fc4417c2d35ce3549c3a563c4277d7ebc8b
tucucrespi <tucucrespi@hotmail.com>
2020-11-05 15:21:27 +0000

    add

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * binary_to_uint - singly linked list
+ * @b: integer
+ *
+ * Description: singly linked list node structure
+ * Return: Holberton project
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int val = 0;
+int i = 0;
+if (b == NULL)
+return 0;
+
+while (b[i] == '0' || b[i] == '1')
+{  // Found another digit.
+val <<= 1;
+val += b[i]-'0';
+i++;
+}
+return val;
+}
