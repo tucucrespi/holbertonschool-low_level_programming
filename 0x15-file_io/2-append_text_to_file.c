742927a89501dd4a89a6dd7caf01e9f7d5383bcd
tucucrespi <tucucrespi@hotmail.com>
2020-11-09 21:37:10 +0000

    add

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - singly linked list
+ * @filename: integer
+ * @text_content: integer
+ * Return: Holberton project
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+int i;
+
+if (filename == NULL && text_content == NULL)
+{
+return (-1);
+}
+fd = open(filename, O_APPEND | O_WRONLY);
+if (fd == -1)
+{
+return (-1);
+}
+for (i = 0; text_content[i] != '\0'; i++)
+{
+}
+if (i == 0)
+{
+close(fd);
+return (1);
+}
+write(fd, text_content, i);
+close(fd);
+return (1);
+}
