825b47fc296900ccb35e5c94808f4de0368c9d6d
tucucrespi <tucucrespi@hotmail.com>
2020-11-09 20:49:53 +0000

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,17 +16,17 @@ if (filename == NULL)
 {
 return (-1);
 }
-fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY | 0600);
+ fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 if (fd == -1)
 {
 return (-1);
 }
-for (i = 0; text_content[i] != '\0'; i++)
-{
-}
 if (text_content == NULL)
 {
 text_content = "";
+}
+for (i = 0; text_content[i] != '\0'; i++)
+{
 }
 write(fd, text_content, i);
 close(fd);
