fd510056ecdf9f568ea70b269acbf1374744c79b
tucucrespi <tucucrespi@hotmail.com>
2020-11-09 21:39:13 +0000

    add

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 int fd;
 int i;
 
-if (filename == NULL && text_content == NULL)
+if (filename == NULL || text_content == NULL)
 {
 return (-1);
 }
