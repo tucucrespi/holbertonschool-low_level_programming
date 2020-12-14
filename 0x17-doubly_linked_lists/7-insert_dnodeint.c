125310e67673a274d0ebf40146b207bc06c5b178
tucucrespi <tucucrespi@hotmail.com>
2020-12-14 22:22:48 +0000

    add

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,38 +10,44 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node;
-dlistint_t *current;
-unsigned int count;
+unsigned int i = 0;
+dlistint_t *aux, *new_node;
 
-
-current = (*h);
-count = 0;
-
+if (!h)
+return (NULL);
+if (idx > dlistint_len(*h))
+return (NULL);
+if (idx == 0 || *h == NULL)
+return (add_dnodeint(h, n));
+if (idx == dlistint_len(*h))
+return (add_dnodeint_end(h, n));
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
-
+aux = *h;
+while (i++ < idx - 1)
+aux = aux->next;
+aux->next->prev = new_node;
+new_node->next = aux->next;
+aux->next = new_node;
 new_node->n = n;
-
-new_node->prev = current->prev;
-current->prev = new_node;
-new_node->next = current;
-
-if (new_node->prev != NULL)
-{
-new_node->prev->next = new_node;
+new_node->prev = aux; 
+return (new_node);
 }
-else
-(*h) = new_node;
 
-while (current)
+/**
+ * add_dnodeint - print all the elements in the list.
+ * @n: abcabcabc
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
 {
-if (count == idx)
-return (current);
-count++;
-current = current->next;
+size_t count = 0;
 
+for (; h; count++)
+{
+h = h->next;
 }
-return (NULL);
+return (count);
 }
