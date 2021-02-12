1670405160c19c60ed4d79afaea853c6a8e85c90
tucucrespi <tucucrespi@hotmail.com>
2021-02-12 20:37:54 +0000

    add

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
 
 
+""" Module 5-island_perimeter"""
+
+
 def island_perimeter(grid):
+    """ function island_perimeter """
+    hg = len(grid)
+    wd = len(grid[0])
+    island = 0
+    edges = 0
 
-    d = 0
-    perimeter = 0
-    height = len(grid)
-    length = len(grid[0])
-    for line in grid:
-        c = 0
-
-        for val in line:
-            if val == 1:
-                surround = 4
-                if c != length - 1:
-                    if grid[d][c + 1] == 1:
-                        surround -= 1
-                        if c != 0:
-                            if grid[d][c - 1] == 1:
-                                surround -= 1
-                                if d != height - 1:
-                                    if grid[d + 1][c] == 1:
-                                        surround -= 1
-                                        if d != 0:
-                                            if grid[d - 1][c] == 1:
-                                                surround -= 1
-                                                perimeter += surround
-                                                c += 1
-                                                d += 1
-        return perimeter
+    for i in range(hg):
+        for j in range(wd):
+            if grid[i][j] == 1:
+                island += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    edges = edges + 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    edges = edges + 1
+    return (island * 4) - (edges * 2)
